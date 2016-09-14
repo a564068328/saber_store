f26909fc387e7d81f29b5cb4046c6afd61b80591
a564068328 <564068328@qq.com>
2016-09-14 11:21:33 +0800

    first commit

---
+++ MyApplication/timber_player/src/main/java/com/naman14/timber/utils/UIUtils.java
@@ -0,0 +1,10 @@
+package com.naman14.timber.utils;
+
+import android.content.Context;
+
+public class UIUtils {
+	public static int dip2px(Context context, float dip) {
+		final float scale = context.getResources().getDisplayMetrics().density;
+		return (int) (dip * scale + 0.5f);
+	}
+}
